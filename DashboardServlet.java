a29a3c0319bfb25c900f743a6a9bb2068f5b29c2
Asantha Unantenne <167331468+Asantha20535@users.noreply.github.com>
2025-05-02 15:57:23 +0530

    Update DashboardServlet.java

--- DashboardServlet.java
+++ DashboardServlet.java
@@ -1,16 +1,32 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+import javax.servlet.RequestDispatcher;
 
 @WebServlet("/DashboardServlet")
 public class DashboardServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
-        // TODO: Implement dashboard logic
-        // 1. Check if user is logged in (session)
-        // 2. Create a list of courses (hardcoded)
-        // 3. Store courses in request attribute
-        // 4. Forward to dashboard.jsp
+
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("username") == null) {
+            response.sendRedirect("index.html");
+            return;
+        }
+
+        List<Course> courses = List.of(
+            new Course("101", "Math", "Dr.Athapattu"),
+            new Course("102", "Dr.Ruwan"),
+            new Course("103", "DS", "Dr.Isuru")
+        );
+
+        request.setAttribute("courses", courses);
+
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("dashboard.jsp");
+        dispatcher.forward(request, response);
     }
 }
